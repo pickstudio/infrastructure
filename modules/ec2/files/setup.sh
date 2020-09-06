5e59793e1ed9b9e5c722aa6c2fa367566601f6f3
DrakeJin <dydwls121200@gmail.com>
2020-09-07 00:47:49 +0900

    feat(AWSami): AMI image를 packer나 anisble을 이용하지 않고서 만드는 방법 연구 AWS Image Builder를 이용할 것

--- modules/ec2/files/setup.sh
+++ modules/ec2/files/setup.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-/root/init_ssh.sh ${github_accounts} >> /root/debug-init_ssh.sh.log 2>&1
+set -e
+
+su ec2-user
+
+GITHUB_ACCOUNTS=${github_accounts}
+
+app () {
+  local authorizedKeyFile='/home/ec2-user/.ssh/authorized_keys'
+  IFS=$'\,'
+  local accounts=($GITHUB_ACCOUNTS)
+  IFS=$' '
+
+  for account in "${accounts[@]}"; do
+    echo $account
+    curl -s https://github.com/$account.keys >> $authorizedKeyFile
+  done
+
+  chown -R ec2-user:ec2-user $authorizedKeyFile
+  chmod 600 $authorizedKeyFile
+}
+
+aws s3 cp s3://pickstudio-secrets/secrets/pickstudio_id_rsa ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+aws s3 cp s3://pickstudio-secrets/secrets/pickstudio_id_rsa.pub ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa.pub
+cat /home/ec2-user/.ssh/id_rsa.pub >> /home/ec2-user/.ssh/authorized_keys
+
+app

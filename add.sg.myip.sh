0dfef2b50cc56b8fd6ef38a10a5a4f8c8b5d9066
DrakeJin <dydwls121200@gmail.com>
2020-09-09 02:09:43 +0900

    방화벽 사용자 등록 스크립트 작성, 이미 등록되어있다면 제거 후 등록이 되도록

---
+++ add.sg.myip.sh
@@ -0,0 +1,23 @@
+SecurityGroupName=members
+Member=$1
+MyIP=$(curl -s ifconfig.co)
+
+SecurityGroupMeta=$(aws ec2 describe-security-groups --filter Name=group-name,Values=${SecurityGroupName} | jq -crM .)
+SecurityGroupId=$(echo "${SecurityGroupMeta}" | jq -crM '.SecurityGroups[0].GroupId')
+SecurityGroupIpPermissions=$(echo $SecurityGroupMeta | jq -crM ".SecurityGroups[0].IpPermissions[] | select( .IpRanges[] | select( .Description == \"${Member}\"))")
+
+if [ "${SecurityGroupIpPermissions}" == "" ]; then
+  echo "이미 제거되었거나, 없습니다. 새로운 ingess를 등록합니다."
+else
+  aws ec2 revoke-security-group-ingress \
+    --group-id ${SecurityGroupId} \
+    --ip-permissions "${SecurityGroupIpPermissions}" | jq -crM
+fi
+
+IngressFormat='{"FromPort":0,"ToPort":0,"IpProtocol":"tcp","IpRanges":[{"CidrIp":"${MYIP}/32","Description":"drake-jin"}],"Ipv6Ranges":[],"PrefixListIds":[],"UserIdGroupPairs":[]}'
+Ingress=$(echo $IngressFormat | jq -crM ".IpRanges[0].CidrIp = \"${MyIP}/32\"")
+
+aws ec2 authorize-security-group-ingress \
+  --group-id ${SecurityGroupId} \
+  --ip-permissions ${Ingress} \
+  | jq -crM

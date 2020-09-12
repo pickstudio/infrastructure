8abda9a108bed7d65a5464212e8aac838c739fde
DrakeJin <dydwls121200@gmail.com>
2020-09-12 18:07:20 +0900

    remove old shit

--- terraform.new/modules/ec2/files/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/bin/bash -c "/root/init_ssh.sh ${github_accounts}"
--- terraform.new/modules/ec2/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip" {
-  value = aws_instance.bastion.public_ip
-}
-
-output "id" {
-  value = aws_instance.bastion.id
-}

ba3514758377b5c602915f8ad636130c3f02f48c
DrakeJin <dydwls121200@gmail.com>
2020-09-12 20:30:43 +0900

    remove old terraform files

--- terraform.old/common/vpc/subnets/private/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "common_vpc_eip_id" {
-  type = string
-}
-
-variable "common_vpc_rt_private" {
-  type = string
-}
--- terraform.old/init/configuration.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_version = "= 0.13.2"
-}
-
-provider "aws" {
-  version = "3.5.0"
-  region  = "ap-northeast-2"
-}

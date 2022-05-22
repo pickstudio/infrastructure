94311dcf7098ac04dfc8a364d461187134dc28d9
drakejin <dydwls121200@gmail.com>
2022-05-22 18:04:09 +0900

    faet: buddystock_data_processor at dev pick ECS

--- terraform/main/common/ecr/buddystock.tf
+++ terraform/main/common/ecr/buddystock.tf
@@ -7,6 +7,7 @@ locals {
 
   repo_name_buddystock_rest = "buddystock_rest"
   repo_name_buddystock_youtube = "buddystock_youtube"
+  repo_name_buddystock_data_processor = "buddystock_data_processor"
 }
 
 resource "aws_ecr_repository" "buddystock_buddystock_rest" {
@@ -49,4 +50,25 @@ resource "aws_ecr_repository" "buddystock_buddystock_youtube" {
 
 output "buddystock_buddystock_youtube" {
   value = aws_ecr_repository.buddystock_buddystock_youtube.repository_url
+}
+
+resource "aws_ecr_repository" "buddystock_data_processor" {
+  name                 = "${local.buddystock_meta.service}/${local.repo_name_buddystock_data_processor}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Crew = local.buddystock_meta.crew
+    Team = local.buddystock_meta.team
+    Service = local.buddystock_meta.service
+    Name = "${local.buddystock_meta.service}/${local.repo_name_buddystock_data_processor}"
+    Repository = "${local.buddystock_meta.service}/${local.repo_name_buddystock_data_processor}"
+  }
+}
+
+output "buddystock_buddystock_data_processor" {
+  value = aws_ecr_repository.buddystock_data_processor.repository_url
 }
